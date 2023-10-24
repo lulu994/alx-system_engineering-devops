756cc43f486046e6a8d4e9d2a001247c12e8210b
lulu994 <loubna.neddah@gmail.com>
2023-10-24 13:00:24 +0200

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,37 @@
 #!/usr/bin/python3
+"""
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+"""
 
-import sys
+import json
 import requests
+from sys import argv
 
-def get_employee_info(employee_id):
-    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        employee_data = response.json()
-        employee_name = employee_data['name']
-        return employee_name
-    else:
-        sys.exit(f"Error: Unable to retrieve employee information for ID {employee_id}.")
 
-def get_todo_list_progress(employee_id):
-    api_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        todos = response.json()
-        total_tasks = len(todos)
-        done_tasks = [todo for todo in todos if todo['completed']]
-        total_done_tasks = len(done_tasks)
-        return total_done_tasks, total_tasks, done_tasks
-    else:
-        sys.exit(f"Error: Unable to retrieve TODO list for employee ID {employee_id}.")
+if __name__ == "__main__":
 
-def display_employee_progress(employee_name, total_done_tasks, total_tasks, done_tasks):
-    print(f"Employee {employee_name} is done with tasks ({total_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t{task['title']}")
+    sessionReq = requests.Session()
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit("Error: Missing employee ID parameter. Please provide an integer ID.")
-    try:
-        employee_id = int(sys.argv[1])
-        employee_name = get_employee_info(employee_id)
-        total_done_tasks, total_tasks, done_tasks = get_todo_list_progress(employee_id)
-        display_employee_progress(employee_name, total_done_tasks, total_tasks, done_tasks)
-    except ValueError:
-        sys.exit("Error: Employee ID must be an integer.")
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    name = employeeName.json()['name']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, totalTasks, len(json_req)))
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))
