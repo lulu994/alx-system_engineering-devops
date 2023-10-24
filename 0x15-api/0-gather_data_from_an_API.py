6a06f0cd7f6a344971316a0cbfcd95d39ef083d5
lulu994 <loubna.neddah@gmail.com>
2023-10-24 12:57:34 +0200

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
+
 import sys
+import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_employee_info(employee_id):
+    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        employee_data = response.json()
+        employee_name = employee_data['name']
+        return employee_name
+    else:
+        sys.exit(f"Error: Unable to retrieve employee information for ID {employee_id}.")
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+def get_todo_list_progress(employee_id):
+    api_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        todos = response.json()
+        total_tasks = len(todos)
+        done_tasks = [todo for todo in todos if todo['completed']]
+        total_done_tasks = len(done_tasks)
+        return total_done_tasks, total_tasks, done_tasks
+    else:
+        sys.exit(f"Error: Unable to retrieve TODO list for employee ID {employee_id}.")
+
+def display_employee_progress(employee_name, total_done_tasks, total_tasks, done_tasks):
+    print(f"Employee {employee_name} is done with tasks ({total_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t{task['title']}")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit("Error: Missing employee ID parameter. Please provide an integer ID.")
+    try:
+        employee_id = int(sys.argv[1])
+        employee_name = get_employee_info(employee_id)
+        total_done_tasks, total_tasks, done_tasks = get_todo_list_progress(employee_id)
+        display_employee_progress(employee_name, total_done_tasks, total_tasks, done_tasks)
+    except ValueError:
+        sys.exit("Error: Employee ID must be an integer.")
