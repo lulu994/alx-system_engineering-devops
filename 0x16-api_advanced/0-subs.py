971f8235fa11dc3aac76d1d07fd4d741a6df299b
lulu994 <loubna.neddah@gmail.com>
2024-01-16 13:26:24 +0100

    Query Reddit API for Subreddit Subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
+
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    Get the number of subscribers for a given subreddit.
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: Number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'custom-user-agent'}  # Set your custom User-Agent
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except KeyError:
+            return 0
     else:
         return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers_count = number_of_subscribers(subreddit)
+        print("{:d}".format(subscribers_count))
