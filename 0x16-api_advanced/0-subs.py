9d6a1cfbfc3afccc187e6dc09929d922cf51d401
lulu994 <loubna.neddah@gmail.com>
2024-01-16 12:40:48 +0100

    Query Reddit API for Subreddit Subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,15 @@ for a given subreddit"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
         data = response.json()
-        return data['data']['subscribers']
+        return data.get('data').get('subscribers')
     else:
         return 0
