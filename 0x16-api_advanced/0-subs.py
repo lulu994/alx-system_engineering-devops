415970ffc72cf39dbfa83f0b46eea769ecb47e94
lulu994 <loubna.neddah@gmail.com>
2024-01-16 13:13:09 +0100

    Query Reddit API for Subreddit Subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
-    if response.status_code == 404:
+    if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
     else:
